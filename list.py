e7ed079edd9eeabb9133eda8b1dae7aa7bae6f40
khanoguz <45708619+khanoguz@users.noreply.github.com>
2020-03-17 11:51:51 +0300

    Delete list.py

--- list.py
+++
@@ -1,9 +0,0 @@
-#booklist = [] or booklist = list()
-
-booklist = ["Ruh Adam","Bozkurtlar Diriliyor","Deli Kurt","Türkçülüğün Esasları"]
-
-new_book = input("please enter new book:")
-booklist+=[new_book]
-
-for i in booklist:
-    print("name of the book: {}".format(i))
